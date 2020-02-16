08ca06b8a02e7c1f1dcd41ec307cad6eb549fe28
Ben Ungar <benungar@hotmail.com>
2020-02-15 22:16:04 -0500

    1

---
+++ Sling/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Goodbye");
+        Application.Quit();
+    }
+
+    public static void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    /**public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }**/
+
+}
---
+++ Sling/Assets/Scripts/PositionSetter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSetter : MonoBehaviour
+{
+    GameObject ball;
+
+    //Vector3 spawnposition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void setSpawnPosition()
+    {
+        //ball.transform.position = 
+    }
+}
